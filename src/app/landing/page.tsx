1ae8924bca3c3e510f2c9614182275f654c5df69
max-b10 <bungaymax@gmail.com>
2024-06-24 11:08:02 +0100

    add types, animations/fadein and landing page

---
+++ src/app/landing/page.tsx
@@ -0,0 +1,13 @@
+import FadeIn from "../animations/FadeIn";
+
+const Landing = () => {
+  return (
+    <FadeIn>
+      <main>
+        <div>Landing</div>
+      </main>
+    </FadeIn>
+  );
+};
+
+export default Landing;
